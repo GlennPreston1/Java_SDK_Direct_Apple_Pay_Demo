b488d33faa28c4168973e39a153c5f3d924b9c1b
peter.shang <peter.shang@nodus.com>
2019-12-26 04:13:08 +0100

    Add Contract Number to the java SDK

--- src/main/java/com/evopayments/turnkey/apiclient/BaseApiCall.java
+++ src/main/java/com/evopayments/turnkey/apiclient/BaseApiCall.java
@@ -19,8 +19,21 @@ import java.util.HashMap;
  */
 public abstract class BaseApiCall extends ApiCall {
 
+	/**
+	 * CARD ON FILE
+	 */
 	public static String SUB_ACTION_COF_FIRST = "SUB_ACTION_COF_FIRST";
 	public static String SUB_ACTION_COF_RECURRING = "SUB_ACTION_COF_RECURRING";
+	/**
+	 * MMRP
+	 */
+	public static String SUB_ACTION_MMRP_FIRST = "SUB_ACTION_MMRP_FIRST";
+	public static String SUB_ACTION_MMRP_RECURRING = "SUB_ACTION_MMRP_RECURRING";
+	/**
+	 * MMRP for Mexico merchant
+	 */
+	public static String SUB_ACTION_MMRP_MX_FIRST = "SUB_ACTION_MMRP_MX_FIRST";
+	public static String SUB_ACTION_MMRP_MX_RECURRING = "SUB_ACTION_MMRP_MX_RECURRING";
 
 	private String subActionType = null;
 
@@ -56,6 +69,8 @@ public abstract class BaseApiCall extends ApiCall {
 		tokenParams.put("country", inputParams.get("country"));
 		tokenParams.put("paymentSolutionId", inputParams.get("paymentSolutionId"));
 		tokenParams.put("merchantNotificationUrl", config.getProperty(MERCHANT_NOTIFICATION_URL_PROP_KEY));
+		tokenParams.put("merchantLandingPageUrl", config.getProperty(MERCHANT_LANDING_PAGE_URL_PROP_KEY));
+
 
 		if(SUB_ACTION_COF_FIRST.equals(this.subActionType)){
 			tokenParams.put("cardOnFileType", "First");
@@ -63,8 +78,44 @@ public abstract class BaseApiCall extends ApiCall {
 			tokenParams.put("cardOnFileType", "Repeat");
 			tokenParams.put("cardOnFileInitiator", "Merchant");
 			tokenParams.put("cardOnFileInitialTransactionId",inputParams.get("cardOnFileInitialTransactionId"));
+		} else if (SUB_ACTION_MMRP_FIRST.equals(this.subActionType)) {
+			tokenParams.put("cardOnFileType", "First");
+			tokenParams.put("mmrpBillPayment", "Recurring");
+			tokenParams.put("mmrpCustomerPresent", "BillPayment");
+		}else if (SUB_ACTION_MMRP_RECURRING.equals(this.subActionType)) {
+			populateParamsForRecurring(tokenParams,inputParams);
+			/**
+			 * MX merchant only?
+			 */
+			tokenParams.put("mmrpOriginalMerchantTransactionId",inputParams.get("mmrpOriginalMerchantTransactionId"));
+		} else if (SUB_ACTION_MMRP_MX_FIRST.equals(this.subActionType)) { // the first payment transaction for MX merchants
+			tokenParams.put("cardOnFileType", "First");
+			tokenParams.put("mmrpBillPayment", "Recurring");
+			tokenParams.put("mmrpCustomerPresent", "BillPayment");
+			tokenParams.put("mmrpContractNumber",inputParams.get("mmrpContractNumber"));
+		}else if (SUB_ACTION_MMRP_MX_RECURRING.equals(this.subActionType)) { // Recurring payment transaction for MX merchants
+			populateParamsForRecurring(tokenParams,inputParams);
+			tokenParams.put("mmrpContractNumber",inputParams.get("mmrpContractNumber"));
+			tokenParams.put("mmrpExistingDebt","NotExistingDebt");
+			tokenParams.put("mmrpCurrentInstallmentNumber","1");
+			tokenParams.put("mmrpOriginalMerchantTransactionId",inputParams.get("mmrpOriginalMerchantTransactionId"));
 		}
+
 		return tokenParams;
+	}
+
+	/**
+	 * Common params required for MMRP
+	 *
+	 * @param tokenParams
+	 * @param inputParams
+	 */
+	private void populateParamsForRecurring(Map<String, String> tokenParams, Map<String, String> inputParams) {
+		tokenParams.put("cardOnFileType", "Repeat");
+		tokenParams.put("cardOnFileInitiator", "Merchant");
+		tokenParams.put("cardOnFileInitialTransactionId",inputParams.get("cardOnFileInitialTransactionId"));
+		tokenParams.put("mmrpBillPayment", "Recurring");
+		tokenParams.put("mmrpCustomerPresent", "BillPayment");
 	}
 
 	@Override
--- src/test/java/com/evopayments/turnkey/apiclient/PurchaseCallTest.java
+++ src/test/java/com/evopayments/turnkey/apiclient/PurchaseCallTest.java
@@ -1,5 +1,6 @@
 package com.evopayments.turnkey.apiclient;
 
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -110,7 +111,6 @@ public class PurchaseCallTest extends  BaseTest{
 	}
 
 
-
 	@Test
 	public void CardOnFileRecurringTestCall(){
 
@@ -148,6 +148,144 @@ public class PurchaseCallTest extends  BaseTest{
 		Assert.assertNotNull(result);
 		Assert.assertEquals("redirection", result.getString("result"));
 	}
+
+	@Test
+	public void MmrpFirstTestCall(){
+		// TOKENIZE
+		final Map<String, String> tokenizeParams = super.buildTokenizeParam();
+
+
+		final TokenizeCall tokenize = new TokenizeCall(config, tokenizeParams, null);
+		final JSONObject tokenizeCall = tokenize.execute();
+
+		// PURCHASE - MMRP
+		final Map<String, String> purchaseParams = new HashMap<>();
+		super.addCommonParams(purchaseParams);
+		purchaseParams.put("amount", "99.0");
+		purchaseParams.put("customerId", tokenizeCall.getString("customerId"));
+		purchaseParams.put("specinCreditCardToken", tokenizeCall.getString("cardToken"));
+		purchaseParams.put("specinCreditCardCVV", "111");
+
+		final PurchaseCall call = new PurchaseCall(config, purchaseParams, null, PurchaseCall.SUB_ACTION_MMRP_FIRST);
+		JSONObject result = call.execute();
+
+		// note that any error will cause the throwing of some kind of SDKException (which extends RuntimeException)
+		// still we make an assertNotNull
+
+		Assert.assertNotNull(result);
+	}
+
+
+
+	@Test
+	public void MmrpRecurringTestCall(){
+/********** Replace the merchantTxId with the transaction id from the MmrpFirstTestCall test case ***/
+		String merchantTxId = SAMPLE_TX_ID;
+//		String merchantTxId = "bV1xrfmRbwkXQBYBeGQr";
+		if(SAMPLE_TX_ID.equals(merchantTxId)) {
+			System.out.println("**** Warning: to run recurring payment, please assign the merchantTxId of the 'First' transaction to variable  [ merchantTxId ] ");
+			return;
+		}
+
+		// TOKENIZE
+		final Map<String, String> tokenizeParams = super.buildTokenizeParam();
+
+		final TokenizeCall tokenize = new TokenizeCall(config, tokenizeParams, null);
+		final JSONObject tokenizeCall = tokenize.execute();
+
+		// PURCHASE (CardOnFile)
+		final Map<String, String> purchaseParams = new HashMap<>();
+		super.addCommonParams(purchaseParams);
+		purchaseParams.put("amount", "97.0");
+		purchaseParams.put("customerId", tokenizeCall.getString("customerId"));
+		purchaseParams.put("specinCreditCardToken", tokenizeCall.getString("cardToken"));
+		purchaseParams.put("specinCreditCardCVV", "111");
+		purchaseParams.put("cardOnFileInitialTransactionId",merchantTxId);
+		purchaseParams.put("mmrpOriginalMerchantTransactionId",merchantTxId);
+
+		final PurchaseCall call = new PurchaseCall(config, purchaseParams, null,PurchaseCall.SUB_ACTION_MMRP_RECURRING);
+		JSONObject result = call.execute();
+
+		// note that any error will cause the throwing of some kind of SDKException (which extends RuntimeException)
+		// still we make an assertNotNull
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals("success", result.getString("result"));
+	}
+
+	@Test
+	public void MmrpFirstMxTestCall(){
+		// TOKENIZE
+		final Map<String, String> tokenizeParams = super.buildTokenizeParam();
+
+
+		final TokenizeCall tokenize = new TokenizeCall(config, tokenizeParams, new PrintWriter(System.out));
+		final JSONObject tokenizeCall = tokenize.execute();
+
+		// PURCHASE - MMRP
+		final Map<String, String> purchaseParams = new HashMap<>();
+		super.addCommonParams(purchaseParams);
+		purchaseParams.put("amount", "200.0");
+		purchaseParams.put("customerId", tokenizeCall.getString("customerId"));
+		purchaseParams.put("specinCreditCardToken", tokenizeCall.getString("cardToken"));
+		purchaseParams.put("specinCreditCardCVV", "111");
+		purchaseParams.put("mmrpContractNumber","1234");
+
+		final PurchaseCall call = new PurchaseCall(config, purchaseParams, null, PurchaseCall.SUB_ACTION_MMRP_MX_FIRST);
+		JSONObject result = call.execute();
+
+		// note that any error will cause the throwing of some kind of SDKException (which extends RuntimeException)
+		// still we make an assertNotNull
+
+		Assert.assertNotNull(result);
+	}
+
+
+
+	/**
+	 * MMRP for MX Merchant
+	 */
+	@Test
+	public void MmrpRecurringMxTestCall(){
+/********** Replace the merchantTxId with the transaction id from the MmrpFirstMxTestCall test case ***/
+		String merchantTxId = SAMPLE_TX_ID;
+//		String merchantTxId = "1yccOOkp1LthnvudBWVw";
+		if(SAMPLE_TX_ID.equals(merchantTxId)) {
+			System.out.println("**** Warning: to run recurring payment, please assign the merchantTxId of the 'First' transaction to variable  [ merchantTxId ] ");
+			return;
+		}
+
+
+		// TOKENIZE
+		final Map<String, String> tokenizeParams = super.buildTokenizeParam();
+
+
+		final TokenizeCall tokenize = new TokenizeCall(config, tokenizeParams, null);
+		final JSONObject tokenizeCall = tokenize.execute();
+
+		// PURCHASE
+		final Map<String, String> purchaseParams = new HashMap<>();
+		super.addCommonParams(purchaseParams);
+		purchaseParams.put("amount", "980.0");
+		purchaseParams.put("customerId", tokenizeCall.getString("customerId"));
+		purchaseParams.put("specinCreditCardToken", tokenizeCall.getString("cardToken"));
+		purchaseParams.put("specinCreditCardCVV", "111");
+		purchaseParams.put("cardOnFileInitialTransactionId",merchantTxId);
+
+		purchaseParams.put("mmrpContractNumber","1234");
+		purchaseParams.put("mmrpOriginalMerchantTransactionId",merchantTxId);
+
+
+		final PurchaseCall call = new PurchaseCall(config, purchaseParams, null,PurchaseCall.SUB_ACTION_MMRP_MX_RECURRING);
+		JSONObject result = call.execute();
+
+		// note that any error will cause the throwing of some kind of SDKException (which extends RuntimeException)
+		// still we make an assertNotNull
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals("success", result.getString("result"));
+	}
+
 
 
 }
