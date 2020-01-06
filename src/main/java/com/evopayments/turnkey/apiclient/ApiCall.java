aa0e98ff8b1f0c40059bc11acb0d3b3dd2a7a4a5
Shiying <shiying.song@nodus.com>
2020-01-06 10:56:06 +0100

    EPSP-999 Merchant SDK about getCashierURL

--- src/main/java/com/evopayments/turnkey/apiclient/ApiCall.java
+++ src/main/java/com/evopayments/turnkey/apiclient/ApiCall.java
@@ -223,7 +223,7 @@ public abstract class ApiCall {
 				if (actionParams == null) {
 					return tokenResponse;
 				}
-				final JSONObject actionResponse;
+				JSONObject actionResponse ;
 				if(actionParams.get("action") == ActionType.GET_CASHIER_URL.getCode()) {
 					actionResponse = postToApi(config.getProperty(BACKOFFICE_OPERATION_ACTION_URL_PROP_KEY), actionParams);
 				} else {
