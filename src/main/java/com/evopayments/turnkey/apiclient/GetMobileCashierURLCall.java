9496dbf8067445e4d5043942fb1b5aef091a57d7
Sokołowski Jakub <jsokolowski@eservice.local>
2020-04-06 16:45:38 +0200

    EPSP-1235 - add custom params to java turnkey sdk

--- src/main/java/com/evopayments/turnkey/apiclient/GetMobileCashierURLCall.java
+++ src/main/java/com/evopayments/turnkey/apiclient/GetMobileCashierURLCall.java
@@ -61,6 +61,26 @@ public class GetMobileCashierURLCall extends ApiCall {
 		tokenParams.put("country", inputParams.get("country"));
 		tokenParams.put("paymentSolutionId", inputParams.get("paymentSolutionId"));
 		tokenParams.put("customerId", inputParams.get("customerId"));
+		tokenParams.put("CustomParameter1Or", inputParams.get("customParameter1Or"));
+		tokenParams.put("CustomParameter2Or", inputParams.get("customParameter2Or"));
+		tokenParams.put("CustomParameter3Or", inputParams.get("customParameter3Or"));
+		tokenParams.put("CustomParameter4Or", inputParams.get("customParameter4Or"));
+		tokenParams.put("CustomParameter5Or", inputParams.get("customParameter5Or"));
+		tokenParams.put("CustomParameter6Or", inputParams.get("customParameter6Or"));
+		tokenParams.put("CustomParameter7Or", inputParams.get("customParameter7Or"));
+		tokenParams.put("CustomParameter8Or", inputParams.get("customParameter8Or"));
+		tokenParams.put("CustomParameter9Or", inputParams.get("customParameter9Or"));
+		tokenParams.put("CustomParameter10Or", inputParams.get("customParameter10Or"));
+		tokenParams.put("CustomParameter11Or", inputParams.get("customParameter11Or"));
+		tokenParams.put("CustomParameter12Or", inputParams.get("customParameter12Or"));
+		tokenParams.put("CustomParameter13Or", inputParams.get("customParameter13Or"));
+		tokenParams.put("CustomParameter14Or", inputParams.get("customParameter14Or"));
+		tokenParams.put("CustomParameter15Or", inputParams.get("customParameter15Or"));
+		tokenParams.put("CustomParameter16Or", inputParams.get("customParameter16Or"));
+		tokenParams.put("CustomParameter17Or", inputParams.get("customParameter17Or"));
+		tokenParams.put("CustomParameter18Or", inputParams.get("customParameter18Or"));
+		tokenParams.put("CustomParameter19Or", inputParams.get("customParameter19Or"));
+		tokenParams.put("CustomParameter20Or", inputParams.get("customParameter20Or"));
 
 		return tokenParams;
 	}
--- src/test/java/com/evopayments/turnkey/apiclient/GetMobileCashierURLCallTest.java
+++ src/test/java/com/evopayments/turnkey/apiclient/GetMobileCashierURLCallTest.java
@@ -11,63 +11,84 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
-public class GetMobileCashierURLCallTest extends  BaseTest{
+import static org.junit.Assert.assertEquals;
 
-	protected Map<String, String> buildTokenizeParam(){
-		Map<String, String> tokenizeParams = super.buildTokenizeParam();
-		tokenizeParams.put("action", "AUTH");
-		tokenizeParams.put("amount", "20.0");
-		tokenizeParams.put("channel", Channel.ECOM.getCode());
-		tokenizeParams.put("country", CountryCode.PL.getCode());
-		tokenizeParams.put("currency", CurrencyCode.PLN.getCode());
-		tokenizeParams.put("paymentSolutionId", "500");
-		tokenizeParams.put("customerId", "8Gii57iYNVSd27xnFZzR");
-		tokenizeParams.put("expiryYear", "21");
+public class GetMobileCashierURLCallTest extends BaseTest {
 
+  protected Map<String, String> buildTokenizeParam() {
+    Map<String, String> tokenizeParams = super.buildTokenizeParam();
+    tokenizeParams.put("action", "AUTH");
+    tokenizeParams.put("amount", "20.0");
+    tokenizeParams.put("channel", Channel.ECOM.getCode());
+    tokenizeParams.put("country", CountryCode.PL.getCode());
+    tokenizeParams.put("currency", CurrencyCode.PLN.getCode());
+    tokenizeParams.put("paymentSolutionId", "500");
+    tokenizeParams.put("customerId", "8Gii57iYNVSd27xnFZzR");
+    tokenizeParams.put("expiryYear", "21");
+    IntStream.range(1, 20)
+        .forEach(
+            counter -> {
+              tokenizeParams.put(
+                  String.format("customParameter%dOr", counter),
+                  String.format("example custom param %d", counter));
+            });
 
-		return tokenizeParams;
-	}
-	/**
-	 * successful case
-	 */
-	@Test
-	public void noExTestCall() {
-		final Map<String, String> tokenizeParams = buildTokenizeParam();
-		final GetMobileCashierURLCall call = new GetMobileCashierURLCall(config, tokenizeParams, null);
-		JSONObject result = call.execute();
+    return tokenizeParams;
+  }
+  /** successful case */
+  @Test
+  public void noExTestCall() {
+    final Map<String, String> tokenizeParams = buildTokenizeParam();
+    final GetMobileCashierURLCall call = new GetMobileCashierURLCall(config, tokenizeParams, null);
+    JSONObject result = call.execute();
 
-		// note that any error will cause the throwing of some kind of SDKException (which extends RuntimeException)
-		// still we make an assertNotNull
+    // note that any error will cause the throwing of some kind of SDKException (which extends
+    // RuntimeException)
+    // still we make an assertNotNull
 
-		Assert.assertNotNull(result);
+    Assert.assertNotNull(result);
+  }
 
-	}
+  /** RequiredParamException test (intentionally left out param) */
+  @Test(expected = TurnkeyValidationException.class)
+  public void reqParExExpTestCall() {
+    try {
 
-	/**
-	 * RequiredParamException test (intentionally left out param)
-	 */
-	@Test(expected = TurnkeyValidationException.class)
-	public void reqParExExpTestCall() {
-		try {
+      final Map<String, String> inputParams = new HashMap<>();
+      inputParams.put("action", "AUTH");
+      inputParams.put("amount", "20.0");
+      inputParams.put("channel", Channel.ECOM.getCode());
+      //			inputParams.put("country", CountryCode.PL.getCode()); // left out field
+      //			inputParams.put("currency", CurrencyCode.PLN.getCode()); // left out field
+      inputParams.put("paymentSolutionId", "500");
 
-			final Map<String, String> inputParams = new HashMap<>();
-			inputParams.put("action", "AUTH");
-			inputParams.put("amount", "20.0");
-			inputParams.put("channel", Channel.ECOM.getCode());
-//			inputParams.put("country", CountryCode.PL.getCode()); // left out field
-//			inputParams.put("currency", CurrencyCode.PLN.getCode()); // left out field
-			inputParams.put("paymentSolutionId", "500");
+      final GetMobileCashierURLCall call = new GetMobileCashierURLCall(config, inputParams, null);
+      call.execute();
 
-			final GetMobileCashierURLCall call = new GetMobileCashierURLCall(config, inputParams, null);
-			call.execute();
+    } catch (TurnkeyValidationException e) {
 
-		} catch (TurnkeyValidationException e) {
+      assertEquals(
+          new TurnkeyValidationException().getTurnkeyValidationErrorDescription()
+              + ":"
+              + Arrays.asList("country", "currency").toString(),
+          e.getMessage());
+      throw e;
+    }
+  }
 
-			Assert.assertEquals(new TurnkeyValidationException().getTurnkeyValidationErrorDescription() + ":" + Arrays.asList("country", "currency").toString(),e.getMessage());
-			throw e;
-
-		}
-	}
-
+  @Test
+  public void testCustomParameters() {
+    final Map<String, String> tokenizeParams = buildTokenizeParam();
+    final GetMobileCashierURLCall call = new GetMobileCashierURLCall(config, tokenizeParams, null);
+    Map<String, String> requestParams = call.getTokenParams(tokenizeParams);
+    IntStream.range(1, 20)
+        .forEach(
+            counter -> {
+              assertEquals(
+                  String.format("example custom param %d", counter),
+                  requestParams.get(String.format("CustomParameter%dOr", counter)));
+            });
+  }
 }
